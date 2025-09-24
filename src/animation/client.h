54955a54fc312631405eae7374da9144d8c259a6
DreamMaoMao <2523610504@qq.com>
2025-09-24 19:35:12 +0800

    feat: add option scratchpad_cross_monitor

--- src/animation/client.h
+++ src/animation/client.h
@@ -891,6 +891,10 @@ void resize(Client *c, struct wlr_box geo, int interact) {
 		c->animainit_geom = c->geom;
 	}
 
+	if (c->isminied) {
+		c->animainit_geom = c->geom;
+	}
+
 	// 开始应用动画设置
 	client_set_pending_state(c);
 
