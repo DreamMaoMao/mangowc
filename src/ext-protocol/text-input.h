bc8d53367fc2ea5e811a02cf5d7941c2d5130f3a
DreamMaoMao <2523610504@qq.com>
2025-10-17 12:41:34 +0800

    chase: chase ime break change for wlroots 0.20
    refer:
    https://gitlab.freedesktop.org/wlroots/wlroots/-/merge_requests/5170#note_3149600

--- src/ext-protocol/text-input.h
+++ src/ext-protocol/text-input.h
@@ -332,7 +332,8 @@ static void handle_keyboard_grab_destroy(struct wl_listener *listener,
 										 void *data) {
 	struct dwl_input_method_relay *relay =
 		wl_container_of(listener, relay, keyboard_grab_destroy);
-	struct wlr_input_method_keyboard_grab_v2 *keyboard_grab = data;
+	struct wlr_input_method_keyboard_grab_v2 *keyboard_grab =
+		relay->input_method->keyboard_grab;
 	wl_list_remove(&relay->keyboard_grab_destroy.link);
 
 	if (keyboard_grab->keyboard) {
