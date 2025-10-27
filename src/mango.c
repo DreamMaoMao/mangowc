31f376f9987d232fbf5be1296f76f00966fce124
DreamMaoMao <2523610504@qq.com>
2025-10-27 12:46:04 +0800

    opt: allow switch focus between fullscreen and floating window in focusstack

--- src/mango.c
+++ src/mango.c
@@ -440,6 +440,7 @@ struct Monitor {
 	int asleep;
 	unsigned int visible_clients;
 	unsigned int visible_tiling_clients;
+	bool has_visible_fullscreen_client;
 	char last_surface_ws_name[256];
 	struct wlr_ext_workspace_group_handle_v1 *ext_group;
 };
@@ -4469,7 +4470,6 @@ void setfullscreen(Client *c, int fullscreen) // 用自定义全屏代理自带�
 		wlr_scene_node_raise_to_top(&c->scene->node); // 将视图提升到顶层
 		resize(c, c->mon->m, 1);
 		c->isfullscreen = 1;
-		// c->isfloating = 0;
 	} else {
 		c->bw = c->isnoborder ? 0 : borderpx;
 		c->isfullscreen = 0;
