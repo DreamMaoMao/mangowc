66b35c36b4505d03e34e8703a1a8e6192a6d99f2
Samq64 <81489795+Samq64@users.noreply.github.com>
2025-12-27 11:01:41 -0500

    Default to TILE instead of SCROLLER

--- src/layout/layout.h
+++ src/layout/layout.h
@@ -17,12 +17,12 @@ static void tgmix(Monitor *m);
 Layout overviewlayout = {"󰃇", overview, "overview"};
 
 enum {
-	SCROLLER,
 	TILE,
 	GRID,
 	MONOCLE,
 	DECK,
 	CENTER_TILE,
+	SCROLLER,
 	VERTICAL_SCROLLER,
 	VERTICAL_TILE,
 	VERTICAL_GRID,
@@ -34,13 +34,13 @@ enum {
 Layout layouts[] = {
 	// 最少两个,不能删除少于两个
 	/* symbol     arrange function   name */
-	{"S", scroller, "scroller", SCROLLER},			 // 滚动布局
 	{"T", tile, "tile", TILE},						 // 平铺布局
 	{"G", grid, "grid", GRID},						 // 格子布局
 	{"M", monocle, "monocle", MONOCLE},				 // 单屏布局
 	{"K", deck, "deck", DECK},						 // 卡片布局
 	{"CT", center_tile, "center_tile", CENTER_TILE}, // 居中布局
 	{"RT", right_tile, "right_tile", RIGHT_TILE},	 // 右布局
+	{"S", scroller, "scroller", SCROLLER},			 // 滚动布局
 	{"VS", vertical_scroller, "vertical_scroller",
 	 VERTICAL_SCROLLER},								   // 垂直滚动布局
 	{"VT", vertical_tile, "vertical_tile", VERTICAL_TILE}, // 垂直平铺布局
